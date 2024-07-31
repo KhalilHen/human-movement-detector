b0f780e86ce13b62038c82bc8cfec4efe0effa94
Khalil <khalilhenane@hotmail.com>
2024-08-01 01:00:08 +0200

    Made that when there is motion detect the buzzer will beep

--- sketch.ino
+++ sketch.ino
@@ -1,38 +1,24 @@
-// Now turn this trash into treasure!
-int buzzerPin13 = 13;
-unsigned long previousMillis = 0;
-const long interval = 60000;
-// unsigned long previousMillis = 0;
-// const long interval = 60000;
-int inputPin = 2;               
-int pirState = LOW;        
-int val = 0;     
+int buzzerPin = 8; 
+const int inputPin = 10; 
+int pirState = LOW; // C
+int val = 0; 
+
 void setup() {
-
-  pinMode(buzzerPin13, OUTPUT);
-pinMode(inputPin, INPUT);   
-  // put your setup code here, to run once:
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(inputPin, INPUT);
   Serial.begin(9600);
-
-
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  val = digitalRead(inputPin);  
-    unsigned long currentMillis = millis();
+  val = digitalRead(inputPin); 
 
-
-    if(val ==  HIGH) {
- if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    tone(buzzerPin13, 1000); 
-    delay(500);
-    noTone(buzzerPin13);
+  if (val == HIGH && pirState == LOW) {       
+    Serial.println("Motion detected!");
+    tone(buzzerPin, 1000); 
+    delay(500); 
+    noTone(buzzerPin); 
+    pirState = HIGH; 
+  } else if (val == LOW) {
+    pirState = LOW; 
   }
-
-  Serial.println("test");
-
-     }
-
-  delay(1); // this speeds up the simulation
 }
