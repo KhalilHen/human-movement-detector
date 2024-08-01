8a8879c0c4de0ba4da2ac2abb2139f09ee8114d6
Khalil <khalilhenane@hotmail.com>
2024-08-02 01:46:33 +0200

    Created a interval that is lower once the person comes closer and added 2 components

--- sketch.ino
+++ sketch.ino
@@ -3,9 +3,16 @@ const int inputPin = 10;
 int pirState = LOW; // C
 int val = 0; 
 
+
+//Distance sensor code
 #define PIN_TRIG 3
 #define PIN_ECHO 2
 
+
+//Push button code
+#define BUTTON_PIN 6
+int oldValue = LOW; 
+boolean turnOffBeep = false;
 void setup() {
   pinMode(buzzerPin, OUTPUT);
   pinMode(inputPin, INPUT);
@@ -13,12 +20,42 @@ void setup() {
     pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
 
+//buttn
+  pinMode(BUTTON_PIN, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
+  int buttonValue = digitalRead(
+    BUTTON_PIN
+  );
 
+
+  if(buttonValue != oldValue)
+  {
+    if(buttonValue == HIGH)
+    {
+
+        if(turnOffBeep == false)
+        {
+
+            turnOffBeep = true;
+    else if (turnOffBeep == true) {
+
+      turnOffBeep = false; 
+    }
+        }
+
+      Serial.println("The button is pressed.");
+    }
+    else
+    {
+      Serial.println("The button is released.");
+    }
+    // Remember the value for the next time.
+    oldValue = buttonValue;
+  }
 
 digitalWrite(PIN_TRIG, HIGH);
   delayMicroseconds(10);
@@ -33,15 +70,29 @@ digitalWrite(PIN_TRIG, HIGH);
   Serial.print("Distance in inches: ");
   Serial.println(duration / 148);
 
-  // Check if distance is less than 100 cm and trigger the buzzer
-  if (distanceCm < 100) {
 
-    tone(buzzerPin, 1000);
+  int beepInterval = map(distanceCm, 0, 100, 200, 1000); 
+  beepInterval = constrain(beepInterval, 1000, 3000); 
+
+
+
+//Comment out to prevent a headache 
+if(turnOffBeep == true) {
+
+Serial.println("Beep is turned off"); 
+
+}
+ else  if (distanceCm < 500) {
+
+    //tone(buzzerPin, 0);
     delay(500);
     noTone(buzzerPin);
-//For now a delay of 5 secs i am later gonna redo it with better interval
-    delay(5000);
+    delay(beepInterval);
   }
+
+ 
+
+
   val = digitalRead(inputPin); 
 
   if (val == HIGH && pirState == LOW  && distanceCm < 100  ) {       
